659b428cd97cae64e9d049b62a67942487e43f83
Brahma Kulkarni <brahma.kulkarni@iiitb.org>
2021-03-13 11:42:32 +0530

    Pushed code for factorial,natural log and power function with tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,6 +7,10 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("Enter the option you want to choose:");
         System.out.println("1. Square Root");
+        System.out.println("2. Factorial (of an integer)");
+        System.out.println("3. Natural logarithm");
+        System.out.println("4. Power function");
+        System.out.println("5. Leave");
 
         Scanner in = new Scanner(System.in);
         int option = in.nextInt();
@@ -17,8 +21,28 @@ public class Calculator {
                 double x = in.nextDouble();
                 System.out.println("The Square Root of "+ x + " is: "+ squareRoot(x));
                 break;
+            case 2:
+                System.out.println("Enter the number (an integer): ");
+                int y = in.nextInt();
+                System.out.println("The Factorial of "+ y + " is: "+ factorial(y));
+                break;
+            case 3:
+                System.out.println("Enter the number: ");
+                double z = in.nextDouble();
+                System.out.println("The Natural Logarithm of "+ z + " is: "+ nLog(z));
+                break;
+            case 4:
+                System.out.println("Enter the base: ");
+                double a = in.nextDouble();
+                System.out.println("Enter the exponent: ");
+                double b = in.nextDouble();
+                System.out.println(a + " raised to the power of "+ b + " is: "+ power(a,b));
+                break;
+            case 5:
+                System.out.println("Fin");
+                break;
             default:
-                System.out.println("Invalid Option");
+                System.out.println("Invalid Option. Please try again.");
         }
     }
 
@@ -27,5 +51,23 @@ public class Calculator {
     public static double squareRoot(double num) {
         logger.info("Performing square root operation on "+num);
         return Math.sqrt(num);
+    }
+
+    public static int factorial(int num) {
+        logger.info("Performing factorial operation on "+num);
+        if (num == 0) {
+            return 1;
+        }
+        return num*factorial(num-1);
+    }
+
+    public static double nLog(double num) {
+        logger.info("Performing natural logarithm operation on "+num);
+        return Math.log(num);
+    }
+
+    public static double power(double x, double y) {
+        logger.info("Performing power operation on "+ x + "," + y);
+        return Math.pow(x,y);
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,11 +1,54 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class CalculatorTest {
     @Test
-    public void sqrtTest() {
+    public void sqrtTestTP() {
         double ret = Calculator.squareRoot(4);
         assertEquals(2,ret, 0.0f);
+    }
+
+    @Test
+    public void sqrtTestTN() {
+        double ret = Calculator.squareRoot(16);
+        assertNotEquals(3,ret, 0.0f);
+    }
+
+    @Test
+    public void factorialTestTP() {
+        int ret = Calculator.factorial(4);
+        assertEquals(24,ret);
+    }
+
+    @Test
+    public void factorialTestTN() {
+        int ret = Calculator.factorial(3);
+        assertNotEquals(7,ret);
+    }
+
+    @Test
+    public void nLogTestTP() {
+        double ret = Calculator.nLog(2.718);
+        assertEquals(1,ret, 0.2f);
+    }
+
+    @Test
+    public void nLogTestTN() {
+        double ret = Calculator.nLog(2.718);
+        assertNotEquals(2,ret, 0.2f);
+    }
+
+    @Test
+    public void powerTestTP() {
+        double ret = Calculator.power(2, 2);
+        assertEquals(4,ret, 0.0f);
+    }
+
+    @Test
+    public void powerTestTN() {
+        double ret = Calculator.power(2, 2);
+        assertNotEquals(5,ret, 0.0f);
     }
 }
